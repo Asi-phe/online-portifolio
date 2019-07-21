03338e05d30845f4eba3b8a794cf2b03f00f774e
Asi-phe <mazamisa.zisu@gmail.com>
2019-07-21 19:16:22 +0200

    Fixing my paragraphs and headings

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+
+
+  // When the user scrolls the page, execute myFunction 
+  window.addEventListener('scroll', myFunctionForSticky);
+
+  // Get the navbar
+  var navbar = document.getElementById("myTopnav");
+
+  // Get the offset position of the navbar
+
+
