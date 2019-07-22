153e2860bfa46f3c3c1b53d075721b046fe32085
Asi-phe <mazamisa.zisu@gmail.com>
2019-07-23 00:00:48 +0200

    adding wrapper

--- js/main.js
+++ js/main.js
@@ -2,12 +2,3 @@
 
 
 
-  // When the user scrolls the page, execute myFunction 
-  window.addEventListener('scroll', myFunctionForSticky);
-
-  // Get the navbar
-  var navbar = document.getElementById("myTopnav");
-
-  // Get the offset position of the navbar
-
-
