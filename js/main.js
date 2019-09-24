9a6372c550191f5a0410b4a6660486cf355b366a
Asi-phe <mazamisa.zisu@gmail.com>
2019-09-24 17:54:02 +0200

    background images

--- js/main.js
+++ js/main.js
@@ -1,52 +1,17 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
 
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-/* Begin jQuery */ ;
-(function ($) {
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-  $(window).scroll(function (e) {
-    if ($(window).scrollTop() >= $("header").height() + 30) {
-      $(".sticky").addClass("fixed");
-      $(".content").addClass("margin");
-
-    } else {
-      $(".sticky").removeClass("fixed");
-      $(".content").removeClass("margin");
-    }
-  e.stopPropagation();
-  
-  });
-
-  // DOM ready
-  $(function () {
-
-    /* Begin Scripts */
-
-    /* Navigation */
-
-    // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function () {
-      $('nav ul').toggle();
-    });
-    // Toggle active CSS class (X - close)
-    document.querySelector("#nav-toggle").addEventListener("click", function () {
-      this.classList.toggle("active");
-    });
-    // If a link has a dropdown, add sub menu toggle.
-    $('nav ul li > a:not(:only-child)').click(function (e) {
-      $(this).siblings('.nav-dropdown').toggle();
-      // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
-      e.stopPropagation();
-    });
-    // Clicking away from dropdown will remove the dropdown class
-    $('html').click(function () {
-      $('.nav-dropdown').hide();
-    });
-
-
-    /* End Scripts */
-
-    /* Everything goes inside these two tags */
-  });
-
-})(jQuery);
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
